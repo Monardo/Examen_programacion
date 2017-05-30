2a494186a67ff0911e784413206a6c5e1a69fca9
Mono <aledrienisienia@gmail.com>
2017-05-30 02:24:12 -0300

    ejercicio 1 del examen con su test

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,3 +1,4 @@
+import unittest
 def devuelveDivisores(a, b):
     divisores = []
     if a == "" or a <= 0:
@@ -8,6 +9,19 @@ def devuelveDivisores(a, b):
                 divisores.append(i)
     return divisores
 
+class tester (unittest.TestCase):
+    def test_1(self):
+        self.assertEqual([], devuelveDivisores("", [1, 2, 3]))
+        self.assertEqual([], devuelveDivisores(-1, [1, 2, 3]))
+        self.assertEqual([], devuelveDivisores(0, [1, 2, 3]))
+        self.assertEqual([], devuelveDivisores(0, []))
+        self.assertEqual([1], devuelveDivisores(1, [1, 2]))
+        self.assertEqual([1, -2], devuelveDivisores(2, [1, -2]))
+        self.assertEqual([1, 2, -4], devuelveDivisores(8, [1, 7, 2, -4, 6, 9]))
+        self.assertEqual([1, 331], devuelveDivisores(331, [1, 2, 3, 7, 147, 331, 518]))
+
+
+""""
 def ejercicio1(var1, var2):
     return devuelveDivisores(var1,var2)
 
@@ -18,4 +32,4 @@ print(ejercicio1(0,[])) # []
 print(ejercicio1(1,[1,2])) # [1]
 print(ejercicio1(2,[1,-2])) # [1,-2]
 print(ejercicio1(8,[1,7,2,-4,6,9])) # [1,2,-4]
-print(ejercicio1(331,[1,2,3,7,147,331,518])) # [1,331]
+print(ejercicio1(331,[1,2,3,7,147,331,518])) # [1,331]"""
