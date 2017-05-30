8ee056a88f14c96591a4b2c82405b2e6ec1541fe
Mono <aledrienisienia@gmail.com>
2017-05-30 02:24:51 -0300

    ejercicio 3 del examen con su test

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,3 +1,4 @@
+import unittest
 def devuelveMatrizTriangular(n):
     import random
     matriz = []
@@ -15,10 +16,22 @@ def devuelveMatrizTriangular(n):
         matrizB.append(matriz)
     return matrizB
 
+class tester (unittest.TestCase):
+    def test_1(self):
+        self.assertEqual([], devuelveMatrizTriangular(-1))
+        self.assertEqual([], devuelveMatrizTriangular(0))
+        self.assertEqual([], devuelveMatrizTriangular(1))
+        #self.assertEqual([[2,3],[0,5]], devuelveMatrizTriangular(2))
+        #self.assertEqual([[x,x,x],[0,x,x],[0,0,x]], devuelveMatrizTriangular(3))
+        #self.assertEqual([[x,x,x,x],[0,x,x,x],[0,0,x,x],[0,0,0,x]], devuelveMatrizTriangular(4))
+        self.assertEqual([], devuelveMatrizTriangular("4"))
+        self.assertEqual([], devuelveMatrizTriangular("PEPE"))
+        self.assertEqual([], devuelveMatrizTriangular(2.5))
+
+
+"""
 def ejercicio3(var1):
     return devuelveMatrizTriangular(var1)
-
-
 print(ejercicio3(-1)) # []
 print(ejercicio3(0)) # []
 print(ejercicio3(1)) # []
@@ -28,3 +41,4 @@ print(ejercicio3(4)) # [[x,x,x,x],[0,x,x,x],[0,0,x,x],[0,0,0,x]]
 print(ejercicio3("4")) #[]
 print(ejercicio3("PEPE")) #[]
 print(ejercicio3(2.5)) #[]
+"""
