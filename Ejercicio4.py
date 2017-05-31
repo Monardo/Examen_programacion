7406b907ab470e78445e4060c6f3857b1a940922
Mono <aledrienisienia@gmail.com>
2017-05-30 23:12:45 -0300

    ejercicio 4

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,4 +1,3 @@
-import unittest
 def devuelveGanador(n):
     ganador = []
     if n == []:
@@ -14,24 +13,3 @@ def devuelveGanador(n):
             ganador = lista[0]
     return ganador
 
-class tester (unittest.TestCase):
-    def test_1(self):
-        self.assertEqual('""', devuelveGanador([]))
-        self.assertEqual('a', devuelveGanador([("a", 1, "b", 0)]))
-        self.assertEqual('c', devuelveGanador([("a",1,"b",0),("a",1,"c",2),("c",3,"b",0)]))
-        self.assertEqual('c', devuelveGanador([("a",1,"b",1),("a",1,"c",1),("c",1,"b",1)]))
-        self.assertEqual('a', devuelveGanador([("a",1,"b",-2),("a",1,"c",1),("c",1,"b",1),("d",1,"a",9)]))
-"""
-def ejercicio4(var1):
-    return devuelveGanador(var1)
-campeonato = []
-1 print(ejercicio4(campeonato)) # ""
-campeonato = [("a",1,"b",0)]
-2 print(ejercicio4(campeonato)) # a
-campeonato = [("a",1,"b",0),("a",1,"c",2),("c",3,"b",0)]
-3 print(ejercicio4(campeonato)) # c
-campeonato = [("a",1,"b",1),("a",1,"c",1),("c",1,"b",1)]
-4 print(ejercicio4(campeonato)) # a  b  c (cualquiera de las 3)
-campeonato = [("a",1,"b",-2),("a",1,"c",1),("c",1,"b",1),("d",1,"a",9)]
-5 print(ejercicio4(campeonato)) # a 
-"""
