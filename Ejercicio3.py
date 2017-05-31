9070fdf6c5531f26dfdfff844d523145ef6a9e69
Mono <aledrienisienia@gmail.com>
2017-05-30 23:12:27 -0300

    ejercicio 3

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,4 +1,3 @@
-import unittest
 def devuelveMatrizTriangular(n):
     import random
     matriz = []
@@ -15,30 +14,3 @@ def devuelveMatrizTriangular(n):
                 matriz.append(x)
         matrizB.append(matriz)
     return matrizB
-
-class tester (unittest.TestCase):
-    def test_1(self):
-        self.assertEqual([], devuelveMatrizTriangular(-1))
-        self.assertEqual([], devuelveMatrizTriangular(0))
-        self.assertEqual([], devuelveMatrizTriangular(1))
-        #self.assertEqual([[2,3],[0,5]], devuelveMatrizTriangular(2))
-        #self.assertEqual([[x,x,x],[0,x,x],[0,0,x]], devuelveMatrizTriangular(3))
-        #self.assertEqual([[x,x,x,x],[0,x,x,x],[0,0,x,x],[0,0,0,x]], devuelveMatrizTriangular(4))
-        self.assertEqual([], devuelveMatrizTriangular("4"))
-        self.assertEqual([], devuelveMatrizTriangular("PEPE"))
-        self.assertEqual([], devuelveMatrizTriangular(2.5))
-
-
-"""
-def ejercicio3(var1):
-    return devuelveMatrizTriangular(var1)
-print(ejercicio3(-1)) # []
-print(ejercicio3(0)) # []
-print(ejercicio3(1)) # []
-print(ejercicio3(2)) # [[x,x],[0,x]]
-print(ejercicio3(3)) # [[x,x,x],[0,x,x],[0,0,x]]
-print(ejercicio3(4)) # [[x,x,x,x],[0,x,x,x],[0,0,x,x],[0,0,0,x]]
-print(ejercicio3("4")) #[]
-print(ejercicio3("PEPE")) #[]
-print(ejercicio3(2.5)) #[]
-"""
