6484f6c09e9ab2e07f705e5e81f4df11926f6e73
Mono <aledrienisienia@gmail.com>
2017-05-30 23:12:03 -0300

    ejercicio 2

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,4 +1,3 @@
-import unittest
 def verificaExtension(a, b):
     nuevab = []
     for i in b:
@@ -17,24 +16,3 @@ def verificaExtension(a, b):
             salida = False
     return salida
 
-class tester (unittest.TestCase):
-    def test_1(self):
-        self.assertEqual(False, verificaExtension('/home/user/listado.txt',['mp3','wav','mpeg']))
-        self.assertEqual(True, verificaExtension('/home/user/listado.txt',['mp3','wav','mpeg','txt']))
-        self.assertEqual(True, verificaExtension('/home/user/listado.txt',['mp3','wav','mpeg','TXT']))
-        self.assertEqual(True, verificaExtension('/home/user/listado.tXt',['mp3','wav','mpeg','TXT']))
-        self.assertEqual(True, verificaExtension('/home/user/listado.txt',['txt']))
-        self.assertEqual(False, verificaExtension('/home/user/listado',['mp3','wav','mpeg','txt']))
-        self.assertEqual(False, verificaExtension('/home/user/listado',[]))
-        self.assertEqual(False, verificaExtension('',[]))
-""""
-def ejercicio2(var1,var2):
-    return verificaExtension(var1,var2)
-print(ejercicio2('/home/user/listado.txt',['mp3','wav','mpeg'])) # False
-print(ejercicio2('/home/user/listado.txt',['mp3','wav','mpeg','txt'])) # True
-print(ejercicio2('/home/user/listado.txt',['mp3','wav','mpeg','TXT'])) # True
-print(ejercicio2('/home/user/listado.tXt',['mp3','wav','mpeg','TXT'])) # True
-print(ejercicio2('/home/user/listado.txt',['txt'])) # True
-print(ejercicio2('/home/user/listado',['mp3','wav','mpeg','txt'])) # False
-print(ejercicio2('/home/user/listado',[])) # False
-print(ejercicio2('',[])) # False"""
